974ec00aee86c5e0fe4d622b22a6026b43b8d110
Carl von Platen <carl@homecomputer.se>
2024-04-17 22:37:11 +0200

    keyplayer: added SysEx parser/converter
    Also changed Program so that the program bank is initially empty
    (INIT_VOICE).

--- keyplayer/Program.h
+++ keyplayer/Program.h
@@ -14,18 +14,24 @@ enum LfoWaveform {
 };
 
 struct LfoParam {
+  LfoParam();
+  
   unsigned char waveform;
   unsigned short delay;
   int deltaPhi;
 };
   
 struct EnvelopeParam {
+  EnvelopeParam(float l0);
+  
   float level0;
   float levels[NUM_ENV_STAGES]; 
   float times[NUM_ENV_STAGES]; 
 };
 
 struct KeyScalingParam {
+  KeyScalingParam();
+
   unsigned char bp;
   bool lcExp;
   signed char lDepth;
@@ -34,6 +40,8 @@ struct KeyScalingParam {
 };
 
 struct FmOperatorParam {
+  FmOperatorParam();
+  
   bool fixedFreq;
   unsigned char velScaling;
   unsigned char kbdRateScaling;
@@ -44,8 +52,11 @@ struct FmOperatorParam {
   KeyScalingParam keyScaling;
 };
 
+struct SyxVoiceParam;
+
 struct Program {
-  const char *name;
+  Program();
+  
   unsigned char algorithm;
   unsigned char feedback;
   float lfoPmDepth;
@@ -53,6 +64,8 @@ struct Program {
   LfoParam lfo;
   EnvelopeParam pitchEnvelope;
   FmOperatorParam op[NUM_OPERATORS];
+
+  void load(const SyxVoiceParam &syxVoice);
 
   static const Program *getProgram(unsigned programNumber);
 };
